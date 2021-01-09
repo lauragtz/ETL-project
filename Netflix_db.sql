c9464d3bc3c651cc45bacd934b24ba849c8e606c
JeremyKalmus <Jeremykalmus@gmail.com>
2021-01-09 12:24:32 -0800

    pulled review data from NYT and added to db

--- Netflix_db.sql
+++ Netflix_db.sql
@@ -23,3 +23,24 @@ CREATE TABLE netflix_movies(
 	user_rating_size TEXT, 
 	movie_or_show TEXT 
 );
+
+CREATE TABLE ny_times_reviews(
+	review_id SERIAL PRIMARY KEY,
+	display_title TEXT, 
+	mpaa_rating TEXT, 
+	critics_pick TEXT,
+	byline TEXT,
+	headline TEXT, 
+	summary_short TEXT, 
+	publication_date TEXT, 
+	opening_date TEXT, 
+	date_updated TEXT, 
+	multimedia TEXT,
+	link_type TEXT,
+	link_url TEXT,
+	link_suggested_link_text TEXT
+);
+
+SELECT * FROM ny_times_reviews
+JOIN netflix_movies 
+ON ny_times_reviews.display_title = netflix_movies.title;
